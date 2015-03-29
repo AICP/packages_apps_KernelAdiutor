f122d7cffe31926b9aeb10741eb9163ac3d3fd1f
Hashbang <hashbang173@gmail.com>
2015-03-29 13:52:15 -0400

    Build with ROM
    Change-Id: I77fd129f3e780e36b23cf96c37078bbf6480f419

---
+++ Android.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    android-support-v4 \
+    android-support-v13 \
+    android-support-v7-cardview \
+    android-support-v7-recyclerview \
+    android-support-v7-appcompat \
+
+
+LOCAL_PACKAGE_NAME := KernelAdiutor
+
+LOCAL_PRIVILEGED_MODULE := true
+
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_PACKAGE)
